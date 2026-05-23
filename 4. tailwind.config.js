54c3e9879d0fd34c0e91d640270927ad148c7210
sulaymesa <sulaymesa@gmail.com>
2026-05-23 09:20:31 -0500

    4. tailwind.config.js

---
+++ 4. tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00b894",
+        secondary: "#0984e3"
+      }
+    }
+  },
+  plugins: []
+}
+``
